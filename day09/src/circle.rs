cef38ff7749e6a7768a6843b076760f768a126b5
J/A <archer884@gmail.com>
2018-12-17 16:32:44 -0600

    I thought I'd make a linked list
    What a stupid plan.

--- day09/src/circle.rs
+++ day09/src/circle.rs
@@ -1,43 +1,72 @@
-use std::collections::VecDeque;
+use std::cell::RefCell;
 
-#[derive(Default)]
-pub struct Circle<T> {
-    current: usize,
-    marbles: VecDeque<T>, // This is probably still a performance shitshow.
+type Arena<'a, T> = typed_arena::Arena<RefCell<Node<'a, T>>>;
+
+// Each time you write a type definition like this one, God kills a whole litter of kittens.
+pub struct Node<'a, T> {
+    l: Option<&'a RefCell<Node<'a, T>>>,
+    r: Option<&'a RefCell<Node<'a, T>>>,
+    item: T,
 }
 
-impl<T> Circle<T> {
-    pub fn push(&mut self, item: T) {
-        self.apply_positive_offset(2);
-        self.marbles.insert(self.current, item);
-    }
-
-    pub fn pop(&mut self) -> Option<T> {
-        self.apply_negative_offset(7);
-        self.marbles.remove(self.current)
-    }
-
-    fn apply_negative_offset(&mut self, offset: usize) {
-        match self.marbles.len() {
-            0 => (),
-            len => {
-                if self.current > offset {
-                    self.current -= offset;
-                } else {
-                    self.current = len - offset - self.current;
-                }
-            }
+impl<'a, T> Node<'a, T> {
+    fn new(item: T) -> Node<'a, T> {
+        Node {
+            l: None,
+            r: None,
+            item,
         }
     }
 
-    fn apply_positive_offset(&mut self, offset: usize) {
-        match self.marbles.len() {
-            0 => (),
-            len => {
-                if self.current + offset < len {
-                    self.current += offset;
-                } else {
-                    self.current = self.current + offset - len;
+    fn append_after_self(&mut self, item: T, arena: &'a mut Arena<'a, T>) {
+        let append = arena.alloc(RefCell::new(Node::new(item)));
+
+        if let Some(mut r) = self.r.as_ref().map(|x| x.borrow_mut()) {
+            append.borrow_mut().l = r.l.take();
+            r.l = Some(append);
+        }
+
+        self.r = Some(append);
+    }
+}
+
+enum Shift {
+    Left(usize),
+    Right(usize),
+}
+
+pub struct Circle<'a, T: 'static> {
+    arena: &'a Arena<'static, T>,
+
+    // It's raining dead kittens.
+    current: Option<&'a RefCell<Node<'a, T>>>,
+    leftmost: Option<&'a RefCell<Node<'a, T>>>,
+    rightmost: Option<&'a RefCell<Node<'a, T>>>,
+}
+
+impl<'a, T> Circle<'a, T> {
+    pub fn new(arena: &'a mut Arena<'static, T>) -> Circle<'a, T> {
+        Circle {
+            arena,
+            current: None,
+        }
+    }
+
+    pub fn push(&mut self, item: T) {
+        self.shift(Shift::Right(1));
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        self.shift(Shift::Left(7));
+
+        unimplemented!()
+    }
+
+    fn shift(&mut self, shift: Shift) {
+        match shift {
+            Shift::Left(n) => {
+                for _ in 0..n {
+                    if let Some(left) = self.current.
                 }
             }
         }
--- day09/src/main.rs
+++ day09/src/main.rs
@@ -3,22 +3,33 @@ mod circle;
 use crate::circle::Circle;
 use hashbrown::HashMap;
 use structopt::StructOpt;
+use typed_arena::Arena;
 
 #[derive(Copy, Clone, Debug, StructOpt)]
 struct Opt {
     pub players: u32,
-    pub last_marble: u32,
+    pub marbles: u32,
 }
 
 fn main() {
-    let opt = Opt::from_args();
+    let Opt { players, marbles } = Opt::from_args();
+    println!("High score: {}", get_high_score(players, marbles));
+}
 
-    let mut circle = Circle::default();
+fn get_high_score(players: u32, marbles: u32) -> u32 {
+    let mut arena = Arena::new();
+    let mut circle = Circle::new(&mut arena);
     let mut scores_by_player = HashMap::new();
 
-    let moves = (0..=opt.last_marble).zip((1..=opt.players).cycle());
+    let moves = (0..=marbles).zip((1..=players).cycle());
 
     for (marble, player) in moves {
+
+        // Just depressing...
+        if (marble + 1) % 10000 == 0 {
+            println!("{}", marble + 1);
+        }
+
         match marble % 23 {
             0 if marble != 0 => {
                 *scores_by_player.entry(player).or_insert(0) += marble + circle.pop().unwrap_or(0);
@@ -28,7 +39,5 @@ fn main() {
         }
     }
 
-    if let Some(score) = scores_by_player.values().max() {
-        println!("High score: {}", score);
-    }
+    scores_by_player.values().cloned().max().unwrap_or_default()
 }
